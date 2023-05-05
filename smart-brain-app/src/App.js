00e3d7ec01506419f7e503df0d96446b920179b1
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-05-05 10:08:21 +0300

    Particles update
    Going straight to main, works as intended

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -3,35 +3,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Particles from 'react-particles';
+import ParticlesBg from 'particles-bg'
 import './App.css';
-
-const particlesOptions = {
-    "particles": {
-      "number": {
-        "value": 50
-      },
-      "size": {
-        "value": 3
-      }
-      },
-    "interactivity": {
-      "events": {
-        "onhover": {
-            "enable": true,
-            "mode": "repulse"
-        }
-      }
-    }
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles
-          params={particlesOptions} 
-        />
+        <ParticlesBg type="square" bg={true} />
         <Navigation />
         <Logo />
         <Rank />

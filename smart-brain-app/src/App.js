1519a1139721c8b8136b9adf0b594126abbf036a
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-05-11 11:12:32 +0300

    7-Can't-Make-Face-Model-Work
    Showed square around face

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -4,9 +4,92 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
 class App extends Component {
+  /* To use state */
+  constructor() {
+    /* To use this. */
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+  
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+  
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input }, () => {
+      // move your Clarifai API call into this callback function
+      const PAT = '41e1940cac9e45a2ae29e33ebc006442';
+      const USER_ID = 'ximaks';       
+      const APP_ID = 'Smart-Brain-App';
+      const MODEL_ID = 'face-detection';
+      const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
+      const IMAGE_URL = this.state.imageUrl;    
+  
+      const raw = JSON.stringify({
+        "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+        },
+        "inputs": [
+          {
+            "data": {
+              "image": {
+                "url": IMAGE_URL
+              }
+            }
+          }
+        ]
+      });
+  
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+        },
+        body: raw
+      };
+  
+      fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions) 
+        .then(response => {
+          console.log(response)
+          return response.json();
+        })
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .then(result => {
+        })
+        .catch(error => console.log('error', error));
+    });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -14,7 +97,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }

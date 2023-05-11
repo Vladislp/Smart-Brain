1d2a228b8fd2a20ed0c8c028d6ba2120c1e29428
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-05-11 11:38:21 +0300

    Update App.js
    Refactor

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
   
 
   displayFaceBox = (box) => {
-    console.log(box)
     this.setState({box: box});
   }
 
@@ -44,50 +43,50 @@ class App extends Component {
   }
   
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input }, () => {
-      // move your Clarifai API call into this callback function
-      const PAT = '41e1940cac9e45a2ae29e33ebc006442';
-      const USER_ID = 'ximaks';       
-      const APP_ID = 'Smart-Brain-App';
-      const MODEL_ID = 'face-detection';
-      const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
-      const IMAGE_URL = this.state.imageUrl;    
+    this.setState({ imageUrl: this.state.input }, this.callClarifaiApi);
+  }
   
-      const raw = JSON.stringify({
-        "user_app_id": {
-          "user_id": USER_ID,
-          "app_id": APP_ID
-        },
-        "inputs": [
-          {
-            "data": {
-              "image": {
-                "url": IMAGE_URL
-              }
+  callClarifaiApi = () => {
+    const PAT = '41e1940cac9e45a2ae29e33ebc006442';
+    const USER_ID = 'ximaks';       
+    const APP_ID = 'Smart-Brain-App';
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
+    const IMAGE_URL = this.state.imageUrl;    
+  
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": IMAGE_URL
             }
           }
-        ]
-      });
-  
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Key ' + PAT
-        },
-        body: raw
-      };
-  
-      fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions) 
-        .then(response => {
-          console.log(response)
-          return response.json();
-        })
-        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-        .then(result => {
-        })
-        .catch(error => console.log('error', error));
+        }
+      ]
     });
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+  
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions) 
+      .then(response => {
+        return response.json();
+      })
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(result => {
+      })
+      .catch(error => console.log('error', error));
   }
   
   render() {

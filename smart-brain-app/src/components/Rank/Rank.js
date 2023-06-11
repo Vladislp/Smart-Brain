0a2108e7d001003d5bce7a6d508f626742418315
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-06-11 16:52:19 +0300

    10-Setup-Server
    Update

--- smart-brain-app/src/components/Rank/Rank.js
+++ smart-brain-app/src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, userEntryCount}) => {
     return (
         <div>
             <div className='white f3'>
-                {'Vladislav, your current rank is...'}
+                {name + ', your current rank is...'}
             </div>
             <div className='white f1'>
-                {'#5'}
+                {userEntryCount}
             </div>
         </div>
 
